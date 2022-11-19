08929654275c3b7cb6b03102d8653ee43cbdb40a
techsamuelsani <samiulsani80@gmail.com>
2022-11-20 00:53:00 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,89 @@
+// Replace if using a different env file or config
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const { resolve } = require("path");
+const session = require("express-session");
+
+const app = express();
+const port = process.env.PORT || 4242;
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2020-08-27',
+  appInfo: { // For sample support and debugging, not required for production:
+    name: "stripe-samples/connect-onboarding-for-standard",
+    version: "0.0.1",
+    url: "https://github.com/stripe-samples"
+  }
+});
+
+app.use(express.static(process.env.STATIC_DIR));
+app.use(
+  session({
+    secret: "Set this to a random string that is kept secure",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.post("/onboard-user", async (req, res) => {
+  try {
+    const account = await stripe.accounts.create({
+      type: 'express',
+    });
+
+    // Store the ID of the new Standard connected account.
+    req.session.accountID = account.id;
+
+    const origin = "http://127.0.0.1:4242";
+    const accountLink = await stripe.accountLinks.create({
+      type: "account_onboarding",
+      account: account.id,
+      refresh_url: `${origin}/onboard-user/refresh`,
+      return_url: `${origin}/success.html`,
+    });
+
+    res.send({
+      url: accountLink.url,
+    });
+    console.log(accountLink.url);
+
+    //res.redirect(303, accountLink.url);
+  } catch (err) {
+    res.status(500).send({
+      error: err.message,
+    });
+  }
+});
+
+app.get("/onboard-user/refresh", async (req, res) => {
+  if (!req.session.accountID) {
+    res.redirect("/");
+    return;
+  }
+
+  try {
+    const { accountID } = req.session;
+    const origin = `${req.secure ? "https://" : "http://"}${req.headers.host}`;
+
+    const accountLink = await stripe.accountLinks.create({
+      type: "account_onboarding",
+      account: accountID,
+      refresh_url: `${origin}/onboard-user/refresh`,
+      return_url: `${origin}/success.html`,
+    });
+
+    res.redirect(303, accountLink.url);
+  } catch (err) {
+    res.status(500).send({
+      error: err.message,
+    });
+  }
+});
+
+app.listen(port, () => console.log(`Node server listening at http://localhost:${port}`));
